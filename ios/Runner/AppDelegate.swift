4791c9e9534d6bfa3d2746b0b71d50b83cb11dfc
Ankitkumar Maisura <ankitmaisuriya.canada@gmail.com>
2026-01-20 13:49:57 +0530

    Created more UI designs

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,16 @@
 import Flutter
 import UIKit
-
+import GoogleMaps
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // ✅ ADD THIS LINE
+    GMSServices.provideAPIKey("AIzaSyATWNcA3ZDb5Fd29dOF2x356gzGiI4KvDM")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
